519a174a908c94a0bd7cb6603cb14044000f9ee3
sofiacottone <cottonesofia@outlook.it>
2024-06-10 16:07:04 +0200

    init project

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script>
+
+</script>
+
+<template>
+  ciao
+</template>
+
+<style lang="scss"></style>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import * as bootstrap from 'bootstrap'
+import App from './App.vue'
+
+createApp(App).mount('#app')
