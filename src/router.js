21472696079718484590eb9e8e54d4faf8f97526
sofiacottone <cottonesofia@outlook.it>
2024-06-11 15:53:06 +0200

    feat: add new route to single project

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import AppHome from './pages/AppHome.vue';
 import ProjectList from './pages/ProjectList.vue';
+import SingleProject from './components/SingleProject.vue';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -15,6 +16,11 @@ const router = createRouter({
             path: '/projects',
             name: 'projects',
             component: ProjectList
+        },
+        {
+            path: '/projects/:slug',
+            name: 'single-project',
+            component: SingleProject
         },
     ]
 });
