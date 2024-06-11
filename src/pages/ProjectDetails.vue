9a20ae1f0fc8fc990249fd8a8c93ee008540a789
sofiacottone <cottonesofia@outlook.it>
2024-06-11 16:56:49 +0200

    feat: add single project details

---
+++ src/pages/ProjectDetails.vue
@@ -0,0 +1,70 @@
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'ProjectDetails',
+    data() {
+        return {
+            project: null
+        }
+    },
+    methods: {
+        getProjectDetails() {
+            axios.get(`http://127.0.0.1:8000/api/projects/${this.$route.params.slug}`)
+                .then((response) => {
+                    this.project = response.data.results;
+                });
+        },
+        changeTechBadgeColor(technologyName) {
+            return {
+                'text-bg-primary': technologyName == 'css',
+                'text-bg-warning': technologyName == 'js',
+                'text-bg-success': technologyName == 'vue',
+                'text-bg-secondary': technologyName == 'sql',
+                'text-bg-dark': technologyName == 'php',
+                'text-bg-danger': technologyName == 'laravel',
+            }
+        },
+    },
+    mounted() {
+        this.getProjectDetails();
+    }
+}
+</script>
+
+<template>
+    <div class="container my-3">
+        <div class="row">
+            <template v-if="project">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h5 class="card-title fw-bold">{{ project.name }}</h5>
+
+                        <div><span class="fw-bold">Client: </span>{{ project.client_name }}</div>
+
+                        <div v-if="project.type"><span class="fw-bold">Type: </span>{{ project.type.name
+                            }}</div>
+
+                        <template v-if="project.technologies.length > 0">
+                            <div class="fw-bold mb-1">Technologies:</div>
+                            <div class="d-flex flex-wrap gap-1">
+                                <div v-for="technology in project.technologies" class="badge"
+                                    :class="changeTechBadgeColor(technology.name)">
+                                    {{ technology.name }}
+                                </div>
+
+                            </div>
+                        </template>
+
+                        <p class="mt-2">{{ project.summary }}</p>
+
+                        <div v-if="project.cover_image" class="w-75 m-auto">
+                            <img :src="`http://127.0.0.1:8000/storage/${project.cover_image}`"
+                                class="card-img-bottom img-fluid" :alt="project.name">
+                        </div>
+                    </div>
+                </div>
+            </template>
+        </div>
+    </div>
+</template>
