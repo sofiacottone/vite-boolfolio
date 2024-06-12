218105058edb014d9c4be6a06071884bb4f04c9e
sofiacottone <cottonesofia@outlook.it>
2024-06-12 18:51:34 +0200

    feat: add error page route

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import AppHome from './pages/AppHome.vue';
 import ProjectList from './pages/ProjectList.vue';
 import ProjectDetails from './pages/ProjectDetails.vue';
+import NotFound from './pages/NotFound.vue';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -21,6 +22,11 @@ const router = createRouter({
             path: '/projects/:slug',
             name: 'single-project',
             component: ProjectDetails
+        },
+        {
+            path: '/:pathMatch(.*)*',
+            name: 'not-found',
+            component: NotFound
         },
     ]
 });
